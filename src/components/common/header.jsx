3c60d71225a6c93a139240a28bc112ba84a0dfc0
applely25 <kimgyuha25@gmail.com>
2025-02-16 02:08:36 +0900

    fear

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,44 +1,75 @@
 import { Arrow_Chevron, Bell } from "@/assets";
-import { selectedSubjectStore, subjectStore } from "@/store/subject";
+import {
+  selectedSubjectStore,
+  selectedSubjectTopicStore,
+  subjectStore,
+} from "@/store/subject";
 import { useAtom } from "jotai";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   const [subjectList, setSubjectList] = useAtom(subjectStore);
+  const [selectedSubjectTopic, setSelectedSubjectTopic] = useAtom(
+    selectedSubjectTopicStore
+  );
   const [selectedSubject, setSelectedSubject] = useAtom(selectedSubjectStore);
 
   const nav = useNavigate();
 
   const onClickSubject = (title) => {
-    nav(`/?subject=${title}`);
+    nav(`?subject=${title}`);
     setSelectedSubject(title);
+    setSelectedSubjectTopic("");
+    setIsOpen(false);
+  };
+  const onClickSubjectTopic = (title, sub) => {
+    nav(`?subject=${title}?sub=${sub}`);
+    setSelectedSubject(title);
+    setSelectedSubjectTopic(sub);
     setIsOpen(false);
   };
 
   return (
-    <div className="w-full absolute top-0 left-0 h-[62px] flex justify-between items-center pl-5 pr-5  z-10">
-      <div className="flex gap-1">
+    <div className="w-full absolute top-0 left-0 h-[62px] flex justify-between items-center pl-5 pr-5  z-10 bg-white">
+      <div className="flex gap-1 relative">
         <p className="text-lg">
-          관심 분야 <b>{selectedSubject}</b>
+          관심 분야{" "}
+          <b>
+            {selectedSubject}{" "}
+            {selectedSubjectTopic && `- ${selectedSubjectTopic}`}
+          </b>
         </p>
-        <div className="relative">
+        <div>
           <Arrow_Chevron
             direction="bottom"
             className="cursor-pointer"
             onClick={() => setIsOpen(!isOpen)}
           />
           {isOpen && (
-            <div className="absolute top-6 left-0 w-max  bg-white border border-gray-300 rounded-md flex flex-col gap-1 p-2">
+            <div className="absolute top-15 left-0 w-[394px] bg-white border border-gray-300 rounded-[34px] flex flex-col gap-1 p-2">
               {subjectList.map((item) => (
-                <p
-                  key={item.id}
-                  onClick={() => onClickSubject(item.title)}
-                  className="cursor-pointer"
-                >
-                  {item.title}
-                </p>
+                <div className="flex gap-[6px]">
+                  <button
+                    className="cursor-pointer bg-[#171717] text-[#F5F5F5] w-[128px] rounded-full text-sm"
+                    onClick={() => onClickSubject(item.title)}
+                  >
+                    {item.title}
+                  </button>
+                  <div className="flex ">
+                    {item.subTopics.map((sub) => (
+                      <button
+                        onClick={() =>
+                          onClickSubjectTopic(item.title, sub.majorTopicTitle)
+                        }
+                        className="cursor-pointer min-w-14 pl-3 pr-3 h-14 rounded-full flex justify-center items-center bg-[#F5F5F5] text-sm color-[#404040]"
+                      >
+                        {sub.majorTopicTitle}
+                      </button>
+                    ))}
+                  </div>
+                </div>
               ))}
             </div>
           )}
--- src/components/pages/HomeMore.jsx
+++ src/components/pages/HomeMore.jsx
@@ -1,26 +1,52 @@
 import { useAtom } from "jotai";
 import HomeLayout from "../common/HomeLayout";
 import { selectedSubjectStore, subjectStore } from "@/store/subject";
+import { Arrow } from "@/assets";
+import newStyled from "@emotion/styled";
 
 const HomeMore = () => {
   const [selectedSubject, setSelectedSubject] = useAtom(selectedSubjectStore);
   const [subjectList, setSubjectList] = useAtom(subjectStore);
 
   return (
-    <HomeLayout
-      title={
-        <>
-          {selectedSubject} 관련 관심사들을 <br />더 자세히 알아보세요
-        </>
-      }
-    >
-      {subjectList.map(
-        (i) =>
-          i.title === selectedSubject &&
-          i.subTopics.map((j) => <div>{j.majorTopicTitle}</div>)
-      )}
-    </HomeLayout>
+    <div className="flex flex-col gap-[10px]">
+      <p className="text-lg font-semibold p-[18px]">
+        {selectedSubject} 관련 관심사들을 <br />더 자세히 알아보세요
+      </p>
+      <div className="flex flex-col">
+        <div className="flex overflow-x-scroll w-[430px] noscroll gap-[6px] pl-[18px] pr-[18px]">
+          {subjectList.map(
+            (i) =>
+              i.title === selectedSubject &&
+              i.subTopics.map((j) => (
+                <Container
+                  bgImg="https://i.pinimg.com/736x/3d/24/ff/3d24ffc760bb39551b687ca294adc072.jpg"
+                  className="w-[194px] h-[146px] p-4 rounded-3xl shrink-0 flex justify-between flex-col text-white"
+                >
+                  <p className="text-sm opacity-80">
+                    {j.majorTopicTitle}에 대한 최신
+                    <br />
+                    정보들을 알려드려요
+                  </p>
+                  <div className="flex justify-between">
+                    <p className="text-[17px]">{j.majorTopicTitle}</p>
+                    <Arrow direction="right" />
+                  </div>
+                </Container>
+              ))
+          )}
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default HomeMore;
+
+const Container = newStyled.div`
+  background: 
+    linear-gradient(0deg, rgba(0, 0, 0, 0.5) 0%, rgba(0, 0, 0, 0.5) 100%),
+    url('${({ bgImg }) => bgImg}');
+  background-size: cover;
+  background-position: center;
+`;
